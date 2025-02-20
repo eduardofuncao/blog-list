e7aab95cdf5151fb5ee8b8239f9f01832750c3ac
Eduardo Função <eduardofuncao@hotmail.com>
2025-02-20 17:34:47 -0300

    only allow usear that created blog to delete the blog -> exercise 4-21

--- controllers/blogs.js
+++ controllers/blogs.js
@@ -67,6 +67,21 @@ blogRouter.put('/:id', async (request, response) => {
 })
 
 blogRouter.delete('/:id', async (request, response) => {
+  const decodedToken = jwt.verify(request.token, process.env.SECRET)
+  if (!decodedToken.id) {
+    return response.status(401).json({
+      error: 'token invalid'
+    })
+  }
+  const blog = await Blog.findById(request.params.id)
+  if (!blog) {
+    return response.status(404).json({error: 'blog not found'})
+  }
+  if (blog.user.toString() !== decodedToken.id.toString()) {
+    return response.status(401).json({error: 'unauthorized'})
+  }
+
+
   await Blog.findByIdAndDelete(request.params.id)
   response.status(204).end()
 })
