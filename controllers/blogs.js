8423033ee2fd82278106e2b3d8adbdcf6f804b95
Eduardo Função <eduardofuncao@hotmail.com>
2025-02-20 16:31:33 -0300

    implements token authentication in login and forces blog creating to be tied to token -> exercise 4-18 and 4-19

--- controllers/blogs.js
+++ controllers/blogs.js
@@ -1,6 +1,15 @@
+const jwt = require('jsonwebtoken')
 const blogRouter = require('express').Router()
 const Blog = require('../models/blog')
 const User = require('../models/user')
+
+const getTokenFrom = request => {
+  const authorization = request.get('authorization')
+  if (authorization && authorization.startsWith('Bearer ')) {
+    return authorization.replace('Bearer ', '')
+  }
+  return null
+}
 
 blogRouter.get('/', async (request, response) => {
   const blogs = await Blog.find({}).populate('user', {username: 1, name: 1})
@@ -17,18 +26,24 @@ blogRouter.get('/:id', async (request, response) => {
 
 blogRouter.post('/', async (request, response) => {
   const body = request.body
-  const user = await User.findById(body.userId)
 
   if (!body.title || !body.url) {
     return response.status(400).json({error: 'url or title missing'})
   } 
+
+  const token = getTokenFrom(request)
+  const decodedToken = jwt.verify(token, process.env.SECRET)
+  if(!decodedToken.id) {
+    return response.status(401).json({error: 'token invalid'})
+  }
+  const user = await User.findById(decodedToken.id)
 
   const newBlog = new Blog({
     title: body.title,
     author: body.author,
     url: body.url,
     likes: body.likes,
-    user: body.userId
+    user: user._id
   })
 
   const savedBlog = await newBlog.save()
